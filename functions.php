58b0304d8a8f60111bb14361dced3ec87530f059
vadimgrig <vgstol@gmail.com>
2023-11-10 10:34:06 +0200

    delete standart JS

--- functions.php
+++ functions.php
@@ -153,10 +153,10 @@ add_action('widgets_init', 'wpstd_widgets_init');
  */
 function wpstd_scripts()
 {
-	wp_enqueue_style('wpstd-style', get_stylesheet_uri(), array(), _S_VERSION);
-	wp_style_add_data('wpstd-style', 'rtl', 'replace');
+	// wp_enqueue_style('wpstd-style', get_stylesheet_uri(), array(), _S_VERSION); //підключає стандартний style.css
+	// wp_style_add_data('wpstd-style', 'rtl', 'replace');
 
-	wp_enqueue_script('wpstd-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
+	// wp_enqueue_script('wpstd-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
 
 	if (is_singular() && comments_open() && get_option('thread_comments')) {
 		wp_enqueue_script('comment-reply');
